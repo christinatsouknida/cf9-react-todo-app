5a8e87af86e1876e5852bf0890cfa6a6a4cfafd5
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-05-21 20:42:25 +0300

    feat(todo): add useTodos hook

---
+++ src/features/todo/types.ts
@@ -0,0 +1,10 @@
+export type Todo = {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+export type TodoFormProps = {
+  // onAdd
+  inputRef: React.RefObject<HTMLInputElement | null>;
+}
